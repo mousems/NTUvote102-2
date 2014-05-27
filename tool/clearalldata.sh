748a0decf886cb0bfbab1e2d918528c1974e021b
MouseMs <mousems.kuo@gmail.com>
2014-05-27 16:44:53 +0800

    clean all data of vote log method

---
+++ tool/clearalldata.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "" > /var/log/NTUvote/votelog.log
+echo "" > /var/log/NTUticket/A1
+echo "" > /var/log/NTUticket/B1
+echo "" > /var/log/NTUticket/B2
+echo "" > /var/log/NTUticket/B3
+echo "" > /var/log/NTUticket/B4
+echo "" > /var/log/NTUticket/B5
+echo "" > /var/log/NTUticket/B6
+echo "" > /var/log/NTUticket/B7
+echo "" > /var/log/NTUticket/B8
+echo "" > /var/log/NTUticket/B9
+echo "" > /var/log/NTUticket/B10
+echo "" > /var/log/NTUticket/C1
+echo "" > /var/log/NTUticket/C2
+echo "" > /var/log/NTUticket/D1
+echo "" > /var/log/NTUticket/D2
+echo "" > /var/log/NTUticket/D3
+echo "" > /var/log/NTUticket/D4
+echo "" > /var/log/NTUticket/D5
