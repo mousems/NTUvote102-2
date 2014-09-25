29efd4642b6b3854bb357e6b549566d25c2bef06
MouseMs <mousems.kuo@gmail.com>
2014-09-25 11:44:32 +0800

    文學院選舉用

--- tool/passgen.php
+++ tool/passgen.php
@@ -14,8 +14,8 @@
 
 	}
 
-	$d1list = array("A","B","C","D","E","F","G","H","I","J","K");
-	$d2list = array(array(0,4)  , array(5,9));
+	$d1list = array("C");
+	$d2list = array(array(0,9) );
 
 
 	foreach ($d1list as $d1list_key => $d1list_value) {
@@ -24,7 +24,7 @@
 		foreach ($d2list as $d2list_key => $d2list_value) {
 			# code...
 
-			for ($i=0; $i < 100; $i++) { 
+			for ($i=0; $i < 3; $i++) { 
 				$result = GenPassword();
 				$d1 = $d1list_value;
 				$d2 = rand($d2list_value[0],$d2list_value[1]);
